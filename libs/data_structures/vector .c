8330e9892a5dcd212b64252157e1a89a5780cb40
crooX <vladickkos20015@mail.ru>
2024-05-11 14:23:06 +0300

    first commit

---
+++ libs/data_structures/vector .c
@@ -0,0 +1,5 @@
+//
+// Created by User on 11.05.2024.
+//
+
+#include "vector .h"
---
+++ libs/data_structures/vector .h
@@ -0,0 +1,8 @@
+//
+// Created by User on 11.05.2024.
+//
+
+#ifndef LAB14_VECTOR_H
+#define LAB14_VECTOR_H
+
+#endif //LAB14_VECTOR_H
